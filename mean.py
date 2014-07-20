9eafaaf132001c573064f732c1bf70fb67daa4bb
Catherine Stables <cstables@med.umich.edu>
2014-07-20 11:58:00 -0400

    newer change change

--- mean.py
+++ mean.py
@@ -5,10 +5,10 @@ import sys
 sum = 2
 n = 0
 
-# Sum input values
+# Sum input values THIS IS NEW
 for num in open('data.txt'): #feeds in file automatically
 	sum += float(num) #new value of sum is sum + float(num)
 	n += 1
 	
 print sum / n
-
+#Newer change change
